a72f5a5bbf35fa887dbb8d4472408f6391ab2324
TITAS MALLICK <44458514+titasmallick@users.noreply.github.com>
2020-02-01 21:29:42 +0530

    google's sw.js codelab with polyfill.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-// we'll version our cache (and learn how to delete caches in
-// some other post)
-const cacheName = 'v1::static';
+importScripts('/cache-polyfill.js');
 
-self.addEventListener('install', e => {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        // '/',
 
-        // CACHE 
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
         '1.jpg',
         '2.jpg',
         '3.jpg',
@@ -22,21 +16,24 @@ self.addEventListener('install', e => {
         'hm.png',
         'hasir mallick favicon.png',
         'logo-02.svg'
-        // CACHE
-      ]).then(() => self.skipWaiting());
-    })
-  );
+     ]);
+   })
+ );
 });
 
-// when the browser fetches a url, either response with
-// the cached object or go ahead and fetch the actual url
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    // ensure we check the *right* cache to match against
-    caches.open(cacheName).then(cache => {
-      return cache.match(event.request).then(res => {
-        return res || fetch(event.request)
-      });
-    })
-  );
+
+self.addEventListener('fetch', function(event) {
+
+console.log(event.request.url);
+
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
