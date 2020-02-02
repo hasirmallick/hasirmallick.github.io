0a8daa9d755cf8582da3c335f9067faab88f0865
titasmallick <44458514+titasmallick@users.noreply.github.com>
2020-02-02 21:22:14 +0530

    offline

--- sw.js
+++ sw.js
@@ -8,3 +8,19 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    }).catch(function() {
+      // If both fail, show a generic fallback:
+      return caches.match('/offline.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
+    })
+  );
+})
