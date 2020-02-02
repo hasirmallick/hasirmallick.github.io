f367ecad459892faf9979bd4167c61092fe81c1f
titasmallick <44458514+titasmallick@users.noreply.github.com>
2020-02-02 21:18:26 +0530

    test

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-var networkDataReceived = false;
-
-startSpinner();
-
-// fetch fresh data
-var networkUpdate = fetch('/data.json').then(function(response) {
-  return response.json();
-}).then(function(data) {
-  networkDataReceived = true;
-  updatePage(data);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function(cache) {
+      return fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    })
+  );
 });
-
-// fetch cached data
-caches.match('/data.json').then(function(response) {
-  if (!response) throw Error("No data");
-  return response.json();
-}).then(function(data) {
-  // don't overwrite newer network data
-  if (!networkDataReceived) {
-    updatePage(data);
-  }
-}).catch(function() {
-  // we didn't get cached data, the network is our last hope:
-  return networkUpdate;
-}).catch(showErrorMessage).then(stopSpinner());
