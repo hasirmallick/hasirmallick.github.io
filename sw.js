ab523a0c190fc71f491b86beb52a738d65c3e3eb
titasmallick <44458514+titasmallick@users.noreply.github.com>
2020-02-07 04:34:27 +0530

    sw.js update

--- sw.js
+++ sw.js
@@ -1,41 +1,27 @@
-importScripts('/cache-polyfill.js');
-
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('Creativegun').then(function(cache) {
-     return cache.addAll([
-        // '/',
-        '/cache-polyfill.js',
-        '1.jpg',
-        '2.jpg',
-        '3.jpg',
-        '4.jpg',
-        '5.jpg',
-        '6.jpg',
-        'favicon-02.png',
-        'fbimg.jpg',
-        'hm.png',
-        'hasir mallick favicon.png',
-        'logo-02.svg'
-     ]);
-   })
- );
+self.addEventListener('install', function(event) {
+	var offlineRequest = new Request('offline.html');
+  event.waitUntil(
+    fetch(offlineRequest).then(function(response) {
+      return caches.open('offline').then(function(cache) {
+        console.log('[oninstall] Cached offline page', response.url);
+        return cache.put(offlineRequest, response);
+      });
+    })
+  );
 });
-
-
 self.addEventListener('fetch', function(event) {
-
-console.log(event.request.url);
-
-event.respondWith(
-
-caches.match(event.request).then(function(response) {
-
-return response || fetch(event.request);
-
-})
-
-);
-
-});
+	 var request = event.request;
+	 if (request.method === 'GET') {
+	 	event.respondWith(
+      fetch(request).catch(function(error) {
+      	console.error(
+          '[onfetch] Failed. Serving cached offline fallback ' +
+          error
+        );
+        return caches.open('offline').then(function(cache) {
+          return cache.match('offline.html');
+        });
+      })
+    );
+  }
+  });
