07796547d88892b018c46333eb71d510f1484a51
titasmallick <44458514+titasmallick@users.noreply.github.com>
2020-02-11 12:10:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
-importScripts('/cache-polyfill.js');
-  var offlineRequest = new Request('offline.html');
-
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('Creativegun').then(function(cache) {
-     return cache.addAll([
-        // '/',
-        'offline.html',
-        '/cache-polyfill.js',
+self.addEventListener('install', function(event) {
+	var offlineRequest = new Request('offline.html');
+  event.waitUntil(
+    fetch(offlineRequest).then(function(response) {
+      return caches.open('offline').then(function(cache) {
+        console.log('[oninstall] Cached offline page', response.url);
+        return cache.addAll([offlineRequest, response,
         'https://fonts.googleapis.com/css?family=Roboto&display=swap',
         'https://fonts.googleapis.com/css?family=Noto+Sans&display=swap',
         'favicon-02.png',
@@ -16,12 +12,11 @@ self.addEventListener('install', function(e) {
         'hm.png',
         'hasir mallick favicon.png',
         'logo-02.svg'
-     ]);
-   })
- );
+          ]);
+      });
+    })
+  );
 });
-
-
 self.addEventListener('fetch', function(event) {
 	 var request = event.request;
 	 if (request.method === 'GET') {
